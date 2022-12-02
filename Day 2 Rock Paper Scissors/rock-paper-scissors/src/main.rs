5f60e49be427115c1dc33aa7157bebb21e85f56e
Alejandro <alejandrobrb210501@gmail.com>
2022-12-02 14:52:12 +0100

    Add part 2 of rock-paper-scissors

--- Day 2 Rock Paper Scissors/rock-paper-scissors/src/main.rs
+++ Day 2 Rock Paper Scissors/rock-paper-scissors/src/main.rs
@@ -1,6 +1,17 @@
 use std::{fs, iter::Enumerate};
 use regex::Regex;
 
+fn win_against(value: &str) -> &str {
+    match value {
+        "Y" => "A",
+        "Z" => "B",
+        "X" => "C",
+        "B" => "X",
+        "C" => "Y",
+        "A" => "Z",
+        _ => "",
+    }
+}
 
 fn lose_against(value: &str) -> &str {
     match value {
@@ -36,20 +47,34 @@ fn get_point_value(value: &str) -> u16 {
 }
 
 fn get_game_score(player: &str, enemy: &str) -> u16 {
+    // Draw
     if player == get_alternative(enemy) {
         return 3;
     }
 
+    // Lose
     if lose_against(player) == enemy {
         return 0;
     } 
 
+    // Win
     6
 }
 
+fn get_player_to_play<'a>(enemy: &'a str, expected_result: &'a str) -> &'a str{
+    match expected_result {
+        "X" => win_against(enemy),
+        "Y" => get_alternative(enemy),
+        "Z" => lose_against(enemy),
+        _ => "",
+    }
+}
+
 fn main() {
+    // Regex just for fun
     let re = Regex::new(r"([A-Z]) ([A-Z])").unwrap();
     let mut total_score = 0;
+    let mut total_score_part_2 = 0;
     let contents = fs::read_to_string("./src/input.txt")
         .expect("Should have been able to read the file");
 
@@ -62,5 +87,16 @@ fn main() {
         }
     });
 
+    contents.split("\n").for_each(|line| {
+        for elem in re.captures_iter(line) {
+            let enemy = &elem[1];
+            let expected_result = &elem[2];
+            let player = get_player_to_play(enemy, expected_result);
+        
+            total_score_part_2 += get_game_score(player, enemy) + get_point_value(player);
+        }
+    });
+
     println!("Score: {}", total_score);
+    println!("Score part 2: {}", total_score_part_2);
 }
