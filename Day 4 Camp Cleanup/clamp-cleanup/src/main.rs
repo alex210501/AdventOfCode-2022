3926028cbc8ffe4cce559e87eaa1740d2399d6fb
Alex <alejandrobrb210501@gmail.com>
2022-12-04 11:50:39 +0100

    Second star for clamp-cleanup

--- Day 4 Camp Cleanup/clamp-cleanup/src/main.rs
+++ Day 4 Camp Cleanup/clamp-cleanup/src/main.rs
@@ -19,16 +19,22 @@ impl Assignment {
         }
     }
 
-    fn is_overlaped(&self, other: &Assignment) -> bool {
+    fn is_fully_overlaped(&self, other: &Assignment) -> bool {
         (self.start <= other.start) && (self.end >= other.end)
+    }
+
+    fn is_overlaped(&self, other: &Assignment) -> bool {
+        ((self.start <= other.start) && (self.end >= other.start)) ||
+        ((self.start <= other.end) && (self.end >= other.end))
     }
 }
 
 fn main() {
-    let reader = BufReader::new(File::open("./src/input.txt").expect("Cannot open sample-input.txt"));
+    let file_path = "./src/input.txt";
 
     /* Part One */
-    let mut overlap = 0;
+    let reader = BufReader::new(File::open(file_path).expect(&format!("Cannot open {}", file_path)));
+    let mut fully_overlap: u16 = 0;
 
     reader.lines().for_each(|line_result| {
         let line = match line_result {
@@ -40,10 +46,29 @@ fn main() {
                                                 .map(|elem| Assignment::from_string(elem))
                                                 .collect();
         
+        if current_pair[0].is_fully_overlaped(&current_pair[1]) || 
+            current_pair[1].is_fully_overlaped(&current_pair[0]) {
+                fully_overlap += 1;
+        }
+    });
+
+    /* Part two */
+    let reader = BufReader::new(File::open(file_path).expect(&format!("Cannot open {}", file_path)));
+    let mut overlap: u16 = 0;
+
+    reader.lines().for_each(|line_result| {
+        let current_pair: Vec<Assignment> = match line_result {
+            Ok(line) => line.split(',')
+                            .map(|elem| Assignment::from_string(elem))
+                            .collect(),
+            Err(_) => return,
+        };
+
         if current_pair[0].is_overlaped(&current_pair[1]) || current_pair[1].is_overlaped(&current_pair[0]) {
             overlap += 1;
         }
     });
 
-    println!("Overlap part one: {}", overlap);
+    println!("Overlap part one: {}", fully_overlap);
+    println!("Overlap part two: {}", overlap);
 }
